7a2d4d89febba64bb7b88a6991c6ffe57bbfd82a
HollowLimbo <hundow160@gmail.com>
2026-09-14 20:49:26 -0300

    urls feitas e exemplos feitos

--- backend/biblioteca_api/alunos/models.py
+++ backend/biblioteca_api/alunos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=150)
+    matricula = models.CharField(max_length=30, unique=True)
+    email = models.EmailField()
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
--- backend/biblioteca_api/emprestimos/models.py
+++ backend/biblioteca_api/emprestimos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from livros.models import Livro
 
-# Create your models here.
+
+class Emprestimo(models.Model):
+
+    livro = models.ForeignKey(
+        Livro,
+        on_delete=models.CASCADE,
+        related_name='emprestimos'
+    )
+
+    aluno_nome = models.CharField(max_length=150)
+    data_emprestimo = models.DateField()
+    data_devolucao = models.DateField(null=True, blank=True)
+    devolvido = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.livro.titulo} - {self.aluno_nome}"
